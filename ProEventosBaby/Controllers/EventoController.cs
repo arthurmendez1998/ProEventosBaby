3285274b6a00c825f083d9fa74d4937fe8792419
arthurmendez1998 <arthur.silva@vectracs.com.br>
2022-11-15 11:16:42 -0300

    Adicionar arquivos de projeto.

---
+++ ProEventosBaby/Controllers/EventoController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventosBaby.Data;
+using ProEventosBaby.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventosBaby.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<EventoModel> _evento = new EventoModel[] { // EventoModel do tipo colleção genérica cria um novo evento
+
+          
+        };
+        private readonly Context _bancocontexto;  // _bancocontexto recebe tudo do bancodedados ou seja, nesse momento, o EventoModel
+        public EventoController(Context bancocontexto) 
+        {
+              _bancocontexto = bancocontexto;
+        }
+        [HttpGet]
+        public IEnumerable<EventoModel> Get()
+        {
+            return this._bancocontexto.Eventos;
+
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<EventoModel> Get(int id)
+        {
+            return this._bancocontexto.Eventos.Where(e => e.EventoId == id);
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    };
+      
+    };
+
---
+++ ProEventosBaby/Data/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProEventosBaby.Models;
+
+namespace ProEventosBaby.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) // o base (options) está passando o options que é um dbcontextOption(vulgo connection string) para o DbContext
+        {
+
+        }
+        public DbSet<EventoModel> Eventos { get; set; }
+    }
+}
